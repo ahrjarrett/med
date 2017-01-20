55d63565662ff38156aa02e5e4bb8295a8c20b3e
Andrew Jarrett <ahrjarrett@gmail.com>
2017-01-20 15:54:28 -0700

    getting ready to add scroll events

---
+++ public/js/scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $(window).scrollTop(0)
+
+  //$('#scrollTop').click(function() {
+  //  $('html, body').animate({ scrollTop: 0 })
+  //  return false
+  //})
+
+})
+
